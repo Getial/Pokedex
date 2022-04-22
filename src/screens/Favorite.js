c893b1327fae6fa9efb2e83d80557fab6b2d594d
Felipe Getial <b.brayan.getial@gmail.com>
2022-04-21 22:56:28 -0400

    funcionalidad de add y remove en el boton de favorites

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { getPokemonsFavoriteApi } from '../api/favorite';
 
 const Favorite = () => {
+  const checkFavorite = async () => {
+    const response = await getPokemonsFavoriteApi();
+    console.log(response);
+  };
   return (
     <SafeAreaView>
       <Text>Favorite</Text>
+      <Button title="Obtener" onPress={checkFavorite} />
     </SafeAreaView>
   );
 };
